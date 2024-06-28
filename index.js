458eebbca5e3355eb9faa81489528a7c25f889f3
Kyle Greenidge <kylegreen149@gmail.com>
2024-06-28 00:21:27 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const noMainId = document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1")
+newHeader.innerHTML = "Kyle is the champion"
+newHeader.setAttribute("id", 'victory')
+document.body.append(newHeader)
